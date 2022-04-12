9bbad47e59dd4c3f25dc9481e5ecfd6d0009e49e
kabaniery <mrscorpio93@gmail.com>
2022-04-12 13:34:44 +0300

    YaEbalEgoRotEbalEgoRotEbalEgoRot
    Add placing products on shop panel. Need calibrate scroll view and add iteraction with products

--- Assets/Scripts/iteractWithShop.cs
+++ Assets/Scripts/iteractWithShop.cs
@@ -7,15 +7,28 @@ public class iteractWithShop : MonoBehaviour
 {
     public GameObject description;
     public GameObject cost;
-    private int countObject = 2;
+    public int countObject = 4;
     private GameObject[,] shopProducts;
+    private Vector2[] productsCoords;
     private void Start()
     {
         shopProducts = new GameObject[countObject, 2];
+        productsCoords = new Vector2[countObject];
         for (int i = 0; i < countObject; i++)
         {
             shopProducts[i, 0] = Instantiate(description, transform, false);
             shopProducts[i, 1] = Instantiate(cost, transform, false);
+            if (i == 0)
+            {
+                productsCoords[i] = new Vector2((shopProducts[i, 0].transform.localPosition.x + shopProducts[i, 1].transform.localPosition.x) / 2, shopProducts[i, 0].transform.localPosition.y);
+            }
+            else
+            {
+                shopProducts[i, 0].transform.localPosition = new Vector2(shopProducts[i - 1, 0].transform.localPosition.x, shopProducts[i - 1, 0].transform.localPosition.y - 250);
+                shopProducts[i, 1].transform.localPosition = new Vector2(shopProducts[i - 1, 1].transform.localPosition.x, shopProducts[i - 1, 1].transform.localPosition.y - 250);
+                productsCoords[i] = new Vector2((shopProducts[i, 0].transform.localPosition.x + shopProducts[i, 1].transform.localPosition.x) / 2, shopProducts[i, 0].transform.localPosition.y);
+                //Тута же будем вписывать цену элементов и т.д. Но так как я в душе не ебу, что за названия, действия и цены у товаров, оставлю их просто разноцветными прямоугольниками
+            }
         }
     }
 }
